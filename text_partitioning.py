20d1221ddd03dbe83c400b9f39b7478a6fdc03ae
jfriduss <jonathan.friduss@gmail.com>
2023-09-04 00:47:34 -0700

    created in the 'text_partitioning.py' file, the function 'reconstruct_ad_w_bolded_skills', and the function it uses on each sentence, 'bold_skills_within_sentence'. This function takes a dictionary with job posting info in it, that has been labelled, and the token classification tokenizer, and outputs the sentence color coded according to label

--- text_partitioning.py
+++ text_partitioning.py
@@ -5,7 +5,7 @@ Created on Mon Aug 21 15:14:37 2023
 @author: jonat_od7omk3
 """
 import re
-
+from training.labelingHelpers.predict_and_label_helpers import group_tuples_by_first_index
 
 def partition_string(input_string):
     sentences = re.split(r'(?<=[.!?])\s+|\n', input_string)
@@ -73,3 +73,68 @@ def reconstruct_description(sentences, helper):
         helper = case_insensitive_replace_substring(helper, str_to_repl, s)
         i += 1
     return helper
+
+
+def reconstruct_ad_w_bolded_skills(d, tok, mode):
+    sentences = d['sentences']
+    recr_posting = d['helpers']
+    labels = d['labels']
+    
+    if mode == 0:
+        #the ad has been hand annotated
+        i = 1
+        for s in sentences: 
+            #there isn't something to bold in this sentence
+            str_to_repl = '[' + str(i) + ']'
+            
+            if labels[i-1][0] == 0:
+                recr_posting = case_insensitive_replace_substring(recr_posting, str_to_repl, s)
+            
+            elif labels[i-1][0] == 1:
+                #there is something to bold in this sentence
+                s_bolded = bold_skills_within_sentence(s, labels[i-1], tok, 1)
+                recr_posting = case_insensitive_replace_substring(recr_posting, str_to_repl, s_bolded)
+                
+            i += 1       
+        
+        print(recr_posting)
+        
+    elif mode == 1:
+        #the ad has not been annotated, and predictions will be made to determine which words are bolded
+        pass
+    
+    
+def bold_skills_within_sentence(sentence, labels, tokenizer, ret_or_print):
+    '''
+    document this. explain how it is similar, but different, than the 'label_skills_in_sentence_and_print' fcn
+    '''
+        
+    tok_sent_obj = tokenizer(sentence, padding = False, truncation = False, return_tensors="pt")
+    
+    helper_l = list(zip(tok_sent_obj.tokens(), tok_sent_obj.word_ids(), labels[1] ))
+    helper_l = [el for el in helper_l if el[1] != None]    
+
+    grouped_helper_l = group_tuples_by_first_index(helper_l)
+    
+    results = []
+    
+    for sublist in grouped_helper_l:
+        concatenated_string = ''    
+
+        for tup in sublist:
+            concatenated_string += tup[0].replace('##', '')        
+        results.append((concatenated_string, sublist[0][2]))
+    
+    bolded_s = ''
+    i = 0
+    for r in results:
+        if (r[1] == 2): bolded_s += r[0]
+        elif (r[1] == 0): bolded_s += "\033[35;5;9m" + r[0] + "\033[0m" #purple
+        elif (r[1] == 1): bolded_s += "\033[33;5;9m" + r[0] + "\033[0m" #yellow
+        bolded_s += ' '        
+        i += 1
+        
+    if ret_or_print == 0:    
+        print(bolded_s)
+    elif ret_or_print == 1:    
+         return bolded_s        
